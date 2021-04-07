dd05d2b492cd445433c373c9bc19a0d0bee6f27c
Sayeed Md. Shaiban <sayeed.k06@gmail.com>
2021-04-07 17:25:42 +0600

    fixed issue related to navbar in deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar(){
     return (
         <div className="navbar">
-            {/* <a href='#'>Company Porfolio</a> */}
+            <a href='/#'>Company Porfolio</a>
             <a href="#about">About</a>
             <a href="#pricing">Pricing</a>
             <a href="#contact">Contact</a>
